766ed443bde595fa4c487536a6322a6471f536f9
SaraGamal2000 <dr.sara.masoud1@gmail.com>
2024-09-08 12:18:19 +0300

    first commit

--- Course iti/flaska_course/flask_proj_day2/app/config.py
+++ Course iti/flaska_course/flask_proj_day2/app/config.py
@@ -1,8 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+import os
 
-class Config():
-    pass
+class Config:
+    SECRET_KEY = os.urandom(32)
 
+    @staticmethod
+    def init_app(app):
+        pass
 class Devconfig(Config):
     DEBUG=True
     SQLALCHEMY_DATABASE_URI="sqlite:///database.db"
@@ -11,9 +15,13 @@ class Prodconfig(Config):
     
     DEBUG=False
     SQLALCHEMY_DATABASE_URI= 'postgresql://flask:123@localhost:5432/posts'
+    UPLOADED_PHOTOS_DEST = 'app/static/'
     
 config_option={
         'dev':Devconfig,
         'prd':Prodconfig
     }
-   
+
+# @staticmethod
+# def init_app(app):
+#     pass
--- Course iti/flaska_course/flask_proj_day2/app/posts/views.py
+++ Course iti/flaska_course/flask_proj_day2/app/posts/views.py
@@ -1,19 +1,22 @@
-from app.model import Post, db
+from app.model import Post, db, User
 from flask import render_template, request, redirect, url_for
 from app.posts import posts_blueprint
-# from crypt import methods
+from app.posts.form import PostForm
+
+# from app.model import User
+
 
 @posts_blueprint.route("/landing", endpoint="land")
 def land():
     posts=Post.query.all()
     # return redirect(url_for('posts_land'))
-    return  render_template("landing.html", posts=posts)
+    return  render_template("posts/landing.html", posts=posts)
 
 
 @posts_blueprint.route("/show/<int:id>", endpoint="show_post")
 def show(id):
     posts = Post.query.get(id)
-    return render_template("show.html", posts=posts)
+    return render_template("posts/show.html", posts=posts)
 
 
 @posts_blueprint.route("/delete/<int:id>", endpoint="delete_post")
@@ -27,15 +30,17 @@ def delete_post(id):
 
 @posts_blueprint.route("/create", endpoint="create_post", methods=['GET', "POST"])
 def create():
+    users=User.query.all()
     if request.method == "POST":
         post = Post(name=request.form['name'],
                     descrip=request.form['descrip'],
-                    image=request.form['image'])
+                    image=request.form['image'],
+                    user_id=request.form['user_id'])
 
         db.session.add(post)
         db.session.commit()
         return redirect(url_for("posts.land"))
-    return render_template("create.html")
+    return render_template("posts/create.html",users=users)
 
 
 @posts_blueprint.route("/edit/<int:id>", endpoint="edit_post", methods=['GET', "POST"])
@@ -45,7 +50,32 @@ def edit(id):
         post.name = request.form['name']
         post.descrip = request.form['descrip']
         post.image = request.form['image']
-
+        post.user_id=request.form['user_id']
         db.session.commit()
         return redirect(url_for("posts.land"))
-    return render_template("edit.html", post=post)
+    return render_template("posts/edit.html", post=post)
+
+
+
+import os
+from werkzeug.utils import secure_filename
+from app.posts.form import PostForm
+@posts_blueprint.route("/for/create", endpoint="Form_Create" ,methods=['GET', "POST"])
+def create_post_form():
+    form = PostForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            if form.image.data:
+                image= form.image.data
+                image_name =secure_filename(image.filename)
+                image_path = os.path.join('static/images/', image_name)
+                image.save(image_path)
+
+            post = Post(name=request.form['name'],
+                    descrip=request.form['descrip'],
+                    image=image_name,
+                    user_id=request.form['user_id'])
+            db.session.add(post)
+            db.session.commit()
+        return redirect(url_for("posts.land"))
+    return render_template("posts/form/FormCreate.html",form=form )
